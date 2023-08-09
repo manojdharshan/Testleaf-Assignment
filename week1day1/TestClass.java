b8a4f4e3f2f3e6f7ec21ecaa2550785bda12f048
manojdharshan <141823790+manojdharshan@users.noreply.github.com>
2023-08-09 15:30:54 +0530

    Add files via upload
    Week1 Day 1 Assignment

---
+++ week1day1/TestClass.java
@@ -0,0 +1,16 @@
+package week1day1;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Selenium");
+		System.out.println("Welcome");
+		
+		//TestClass obj = new TestClass();
+		
+
+	}
+
+}
